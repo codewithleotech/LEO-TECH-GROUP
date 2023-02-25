a088adb020fab0e27cbbb48d4e04ad50f4218457
Hoang Tra <traminhoang@gmail.com>
2023-02-25 23:50:29 +0700

    LEO TECH GROUP

---
+++ index.js
@@ -0,0 +1,12 @@
+const leotech_group = 'LEO TECH GROUP';
+const ecosystem = {
+    'LEO TECH': 0,
+    'LEO TECH X': 1,
+    'LEO TECH ASIA: Development Center Customer Services': 2,
+    'LEO TECH ACADEMY: All Courses Programmings': 3,
+    'LEO TECH ONLINE: Real al Products Go Live at LEO TECH': 4,
+    'LAB LEO TECH: Development Center all Products at LEO TECH': 5,
+    'PASS LEO TECH: HEADHUNTERS HR': 6,
+}
+const value = ecosystem[leotech_group]
+    ?? -1
